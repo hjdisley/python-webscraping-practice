914fc783c27bcba807bd021e8b673ac85824e159
Hayden Disley <haydendisley@haydens-mbp.home>
2021-05-03 23:05:22 +0100

    get titles and job data

--- main.py
+++ main.py
@@ -5,9 +5,31 @@ url = "https://uk.indeed.com/jobs?q=junior+developer&l=United+Kingdom"
 
 response = requests.get(url)
 
-data = response.text 
+data = response.text
 
 soup = BeautifulSoup(data, "html.parser")
 
 tags = soup.find_all("a")
 
+# for tag in tags:
+#     print(tag.get('href'))
+
+titles = soup.find_all("a", {"class": "jobtitle"})
+
+for title in titles:
+    print(title.text)
+
+locations = soup.find_all("span", {"class": "location"})
+
+for location in locations:
+    print(location.text)
+
+companies = soup.find_all("span", {"class": "company"})
+
+for company in companies:
+    print(company.text)
+
+summaries = soup.find_all("div", {"class": "summary"})
+
+for summary in summaries:
+    print(summary.text)
